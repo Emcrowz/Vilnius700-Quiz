68f496f82609847a711a5433ef72e2f5a1e683ad
Martynas <martynas.vru@outlook.com>
2023-08-02 11:52:42 +0300

    small update

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -3,7 +3,7 @@ import styles from "./StartScreen.module.css";
 function StartScreen({ topic, numQuestions, dispatch }) {
   return (
     <div className={styles.start}>
-      <h2>Welcome to the Quiz!</h2>
+      <h2>Welcome to the quiz about {topic}!</h2>
       <h3>
         Our quiz contains {numQuestions} questions to see how good you know
         history and related information about {topic}!
