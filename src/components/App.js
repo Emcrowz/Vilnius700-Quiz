ce0935766043fbc635b65062d7bd42e077acce55
Martynas <martynas.vru@outlook.com>
2023-08-07 21:51:17 +0300

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -105,7 +105,7 @@ function App() {
   );
 
   useEffect(function () {
-    fetch("../data-local/questions.json")
+    fetch("http://localhost:8000/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
