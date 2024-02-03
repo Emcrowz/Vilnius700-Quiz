94492e5ef407159ae6df58ad8b16cea66dbe3743
Martynas V <martynas.vru@outlook.com>
2024-02-03 10:28:49 +0000

    Layout changes and fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import styles from "./Header.module.css";
 function Header() {
   return (
     <header className={styles.appHeader}>
-      <h2>Literatūrinis protmūšis!</h2>
+      <h2 className={styles.title}>Literatūrinis protmūšis!</h2>
       {/* <img
         src={require(`../assets/VCB_Logo_no_bg.png`)}
         alt="Vilnius 700 logo"
