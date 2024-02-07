680c565ec6f6eb084942c09a07390d281ee4951f
Martynas <martynas.vru@outlook.com>
2024-02-07 14:19:39 +0200

    StrictMode back

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React>
+  <React.StrictMode>
     <App />
-  </React>
+  </React.StrictMode>
 );
